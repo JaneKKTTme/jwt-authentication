9d1c63f22f691b3e078ef76d49f391a3f7869172
Jane Khomenko <eva.khomenko.28@mail.ru>
2026-05-12 00:19:49 +0300

    Refactor(tests): reuse seed scripts in conftest to eliminate duplication

---
+++ scripts/__init__.py
@@ -0,0 +1,9 @@
+from .seed_permissions import seed_permissions, seed_roles, assign_roles_to_users
+from .seed_users import seed_users
+
+__all__ = [
+    'seed_permissions',
+    'seed_roles', 
+    'assign_roles_to_users',
+    'seed_users',
+]
